8b5c01b93a2e81b52f0e63c5558665a2f061be08
Eduardo Mendieta <eduardo.mendieta.t@gmail.com>
2022-05-10 12:27:32 -0500

    Creando el proyecto

---
+++ src/main/java/com/vncarca/VncarcaApplication.java
@@ -0,0 +1,13 @@
+package com.vncarca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VncarcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VncarcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vncarca/VncarcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vncarca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VncarcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
