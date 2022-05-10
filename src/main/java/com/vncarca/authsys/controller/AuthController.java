0e28e1dcc295344fd2cc2a67123585cc2010a5a6
Willkill1512 <95000835+Willkill1512@users.noreply.github.com>
2022-05-10 18:21:29 -0500

    cambio en cross origin
    cambio

--- src/main/java/com/vncarca/authsys/controller/AuthController.java
+++ src/main/java/com/vncarca/authsys/controller/AuthController.java
@@ -34,7 +34,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+//@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin ({"*"})
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
