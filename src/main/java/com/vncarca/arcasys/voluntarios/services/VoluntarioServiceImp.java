9bcaf318c893e0568d3807ce7c39c7b940149608
Willkill1512 <95000835+Willkill1512@users.noreply.github.com>
2022-06-22 00:03:54 -0500

    Arreglando errores
    Voluntarios

--- src/main/java/com/vncarca/arcasys/voluntarios/services/VoluntarioServiceImp.java
+++ src/main/java/com/vncarca/arcasys/voluntarios/services/VoluntarioServiceImp.java
@@ -1,11 +1,15 @@
 package com.vncarca.arcasys.voluntarios.services;
 
+import org.aspectj.weaver.patterns.PerObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.vncarca.arcasys.persona.model.Persona;
+import com.vncarca.arcasys.persona.repository.PersonaRepository;
 import com.vncarca.arcasys.voluntarios.model.Voluntario;
+import com.vncarca.arcasys.voluntarios.model.VoluntarioDto;
 import com.vncarca.arcasys.voluntarios.repository.VoluntarioRepository;
 
 @Service
@@ -14,6 +18,9 @@ public class VoluntarioServiceImp implements VoluntarioService {
     @Autowired
     VoluntarioRepository voluntariorepository;
 
+    @Autowired
+    private PersonaRepository personaRepository;
+    
     @Override
     public Page<Voluntario> findAll(Pageable pageable) {
         return voluntariorepository.findAll(pageable);
@@ -24,14 +31,46 @@ public class VoluntarioServiceImp implements VoluntarioService {
         return voluntariorepository.findById(id).orElse(null);
     }
 
-    @Override
-    public Voluntario save(Voluntario voluntario) {
-        return voluntariorepository.save(voluntario);
-    }
+    
 
     @Override
     public void delete(Long id) {
         voluntariorepository.deleteById(id);
+    }
+
+    @Override
+    public Voluntario findByCedula(String cedulaPersona) {
+        
+        if(personaRepository.existsByCedula(cedulaPersona)){
+            Persona persona = personaRepository.findByCedula(cedulaPersona).get();
+            Voluntario voluntario = voluntariorepository.findByPersona(persona).orElse(null);
+            return voluntario;
+        }
+        return null;
+    }
+
+    @Override
+    public Voluntario save(VoluntarioDto voluntarioDto, Long idPersona) {
+        Persona persona = personaRepository.findById(idPersona).orElse(null);
+        if (persona != null) {
+            Voluntario voluntario = new Voluntario();
+            voluntario.setActividad(voluntarioDto.getActividad());
+            voluntario.setPersona(persona);
+            voluntario.setTipo(voluntarioDto.getTipo());
+            return voluntariorepository.save(voluntario);
+        }
+        return null;
+    }
+
+    @Override
+    public Voluntario update(VoluntarioDto voluntarioDto, Long idVoluntario) {
+        Voluntario voluntario = voluntariorepository.findById(idVoluntario).orElse(null);
+        if (voluntario != null) {
+            voluntario.setActividad(voluntarioDto.getActividad());
+            voluntario.setTipo(voluntarioDto.getTipo());
+            return voluntariorepository.save(voluntario);
+        }
+        return null;
     }
     
 }
